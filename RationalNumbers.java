1cd20fe1219a6cfecedf13b95321d0b1129ac3f8
Devang Doshi <doshidev@gmail.com>
2016-10-28 00:40:49 -0400

    Assignment 2 (updated)

--- RationalNumbers.java
+++ RationalNumbers.java
@@ -1,163 +1,118 @@
 /*
  * File:    P1DeterminePi.java
- * Date:    Oct 25, 2016
+ * Date:    Oct 27, 2016
  * Student: Devang Doshi | UCID: dbd24
  * Course:  Programming, Data Structure, and Alogrithms | Section: CS50581 
- * Desc:    The program will accept rational numbers (floats) and convert them 
+ * Desc:    The program will test rational numbers (floats) and convert them 
  *          into a fractions. It will then perform basic aritmethic operations
  *          two fractions i.e. add, subtract, multiplication, and division.
- * Usage:   The program prompts user to enter two fraction of upto two decimals
- *          and prints the fraction and the result of the four arithmetic
- *          operations. 
- */
-
+ 
 /**
  *
  * @author Devang
  */
 
-import java.util.Scanner;
-
  public class RationalNumbers {
-     public static void main(String[] args){
-       Rational a = new Rational(getDouble("Enter 1st double value:")); 
-       Rational b = new Rational(getDouble("Enter 2nd double value:"));
-       
-       System.out.println("Addition: " + a.add(b).toString());
-       System.out.println("Addition: " + a.add(b).toDouble());
-       
-       System.out.println();
-              
-       System.out.println("Subtraction: " + a.subtract(b).toString());
-       System.out.println("Subtraction: " + a.subtract(b).toDouble());
-       
-       System.out.println();
-              
-       System.out.println("Multiplication: " + a.multiply(b).toString());
-       System.out.println("Multiplication: " + a.multiply(b).toDouble());
-       
-       System.out.println();
-              
-       System.out.println("Division: " + a.divide(b).toString());
-       System.out.println("Division: " + a.divide(b).toDouble());
-       
-     }
      
-     
-     /** This function get a double input from the user
-     * @param question
-     * @return double input from the user
-    */
-   public static double getDouble(String question){
-        System.out.println(question);
-        Scanner input = new Scanner(System.in);
-        double userInput = input.nextDouble();
-        return userInput;
+     public static void main (String[] args) {
+       
+       test(new Rational(1.2), new Rational(2.4));
+       test(new Rational(10.7), new Rational(5.8));
+       test(new Rational(195.2), new Rational(287.4));
+       test(new Rational(-5.6), new Rational(-2.1));
+       
+     }   
+   
+   public static void test(Rational one, Rational two) {
+       System.out.println ("[" + one.toDouble() + ", " + two.toDouble() + "]");
+       System.out.format ("%8s : %s + %s = %s = %.2f%n", "Add", one.toString(), two.toString(), one.add(two).toString(), one.add(two).toDouble());
+       System.out.format ("%8s : %s - %s = %s = %.2f%n", "Subtract", one.toString(), two.toString(), one.subtract(two).toString(), one.subtract(two).toDouble());
+       System.out.format ("%8s : %s x %s = %s = %.2f%n", "Multiply", one.toString(), two.toString(), one.multiply(two).toString(), one.multiply(two).toDouble());
+       System.out.format ("%8s : %s / %s = %s = %.2f%n", "Divide", one.toString(), two.toString(), one.divide(two).toString(), one.divide(two).toDouble());
+       System.out.println();
    }
 }
-
-class Rational{
-    // Local Variables
+ 
+class Rational {
     private int numerator, denominator;
     
-    // Constructor
-    public Rational(double frac) {
-        createFraction(frac);
+    public Rational (double frac) {
+        createFraction (frac);
     }
     
     //Public Method add
-    public Rational add(Rational y){
-        Rational result = new Rational(0.0);    
-        int factor;
-        
-        result.numerator =((this.numerator * y.denominator) + (this.denominator * y.numerator));
-        result.denominator =(this.denominator * y.denominator);
-        
-        factor = gcf(result.numerator, result.denominator);
-        result.numerator = (int) (result.numerator / factor);
-        result.denominator = (int) (result.denominator / factor);
-        
+    public Rational add (Rational y) {
+        Rational result = new Rational (0.0);
+        result.numerator = (this.numerator * y.denominator) + (this.denominator * y.numerator);
+        result.denominator = this.denominator * y.denominator;
+        result.simplify ();
         return result;
     }
-    
     
     //Public Method Subtract
-    public Rational subtract(Rational y){
-        Rational result = new Rational(0.0);    
-        int factor;
-        
-        result.numerator =((this.numerator * y.denominator) - (this.denominator * y.numerator));
-        result.denominator =(this.denominator * y.denominator);
-        
-        factor = gcf(result.numerator, result.denominator);
-        result.numerator = (int) (result.numerator / factor);
-        result.denominator = (int) (result.denominator / factor);
-        
+    public Rational subtract (Rational y) {
+        Rational result = new Rational (0.0);
+        result.numerator = (this.numerator * y.denominator) - (this.denominator * y.numerator);
+        result.denominator = this.denominator * y.denominator;
+        result.simplify ();
         return result;
     }
     
-    
     //Public Method Multiplication
-    public Rational multiply(Rational y){
-        Rational result = new Rational(0.0);    
-        int factor;
-        
-        result.numerator =(this.numerator * y.numerator);
-        result.denominator =(this.denominator * y.denominator);
-        
-        factor = gcf(result.numerator, result.denominator);
-        result.numerator = (int) (result.numerator / factor);
-        result.denominator = (int) (result.denominator / factor);
-        
+    public Rational multiply (Rational y) {
+        Rational result = new Rational (0.0);
+        result.numerator = this.numerator * y.numerator;
+        result.denominator = this.denominator * y.denominator;
+        result.simplify ();
         return result;
     }
     
     //Public Method Division
-    public Rational divide(Rational y){
-        Rational result = new Rational(0.0);    
-        int factor;
-        
-        result.numerator =(this.numerator * y.denominator);
-        result.denominator =(this.denominator * y.numerator);
-        
-        factor = gcf(result.numerator, result.denominator);
-        result.numerator = (int) (result.numerator / factor);
-        result.denominator = (int) (result.denominator / factor);
-        
+    public Rational divide (Rational y) {
+        Rational result = new Rational (0.0);
+        result.numerator = this.numerator * y.denominator;
+        result.denominator = this.denominator * y.numerator;
+        result.simplify ();
         return result;
     }
     
-    
     //Public toDouble
-    public double toDouble(){
-        return ((double)this.numerator / this.denominator);
+    public double toDouble () {
+        return (double) this.numerator / this.denominator;
     }
     
     //Public toString
-    public String toString(){
-        return ("[" + this.numerator +" / " + this.denominator + "]");
+    public String toString () {
+        return ("[" + this.numerator + " / " + this.denominator + "]");
     }
     
-    //Private Method: Find Greates Common Factor
-    private void createFraction(double frac){
+    //Private Method: Create Fraction
+    private void createFraction (double frac) {
         int[] result = new int[2];
-        String fstring = String.valueOf(frac);
+        String fstring = String.valueOf (frac);
         int decimals;
         
         decimals = fstring.length() - 1 - fstring.indexOf('.') ;
         
         this.numerator = (int) (frac * Math.pow(10, decimals));
         this.denominator = (int) (1 * Math.pow(10, decimals));
+        this.simplify();
     }
     
     //Private Method: Find Greatest Common Factor
-    private int gcf(int one, int two){
-        
-        if(one == two || two == 0)
+    private int gcf (int one, int two) {
+        if (one == two || two == 0)
             return one;
-        else if(one < two)
-            return gcf(one, two % one);
         else
-            return gcf(two, one % two);
-    }   
+            return gcf (two, one % two);
+    }
+    
+    //Private Method: Simplify
+    private void simplify () {
+        if (this.numerator !=0 && this.denominator != 0) {
+            int factor = gcf (this.numerator, this.denominator);
+            this.numerator /= factor;
+            this.denominator /= factor;
+        }
+    }
 }
